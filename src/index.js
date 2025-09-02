a41edaecf4d6a61087e8903400694af99f4c4701
Atikin132 <nikita.kupriyan0v@yahoo.com>
2025-09-02 12:25:26 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-/*
 const MORSE_TABLE = {
   '.-': 'a',
   '-...': 'b',
@@ -37,8 +36,68 @@ const MORSE_TABLE = {
   '----.': '9',
   '-----': '0',
 };
-*/
 
-module.exports = function decode(/* expr */) {
-  throw new Error('Not implemented');
+module.exports = function decode(expr) {
+  const words = expr.split('**********');
+  const splitArrs = [];
+  const morseArrs = [];
+  for (let i = 0; i < words.length; i += 1) {
+    splitArrs.push(
+      words[i]
+        .split(/(\d{2})/)
+        .filter((x) => x !== '')
+        .map((x) => {
+          if (x === '00') {
+            return '';
+          }
+          if (x === '10') {
+            return '.';
+          }
+          if (x === '11') {
+            return '-';
+          }
+          return x;
+        })
+    );
+    let str = '';
+    for (let j = 0; j < splitArrs[i].length; j += 1) {
+      if (splitArrs[i][j] !== '') {
+        str = `${str}${splitArrs[i][j]}`;
+      } else if (j !== 0 && splitArrs[i][j] === '') {
+        morseArrs.push(str);
+        str = '';
+      }
+      if (j === splitArrs[i].length - 1) {
+        morseArrs.push(str);
+        str = '';
+      }
+    }
+    morseArrs.push(' ');
+  }
+  let res = '';
+  morseArrs
+    .filter((x) => x !== '')
+    .map((x) => {
+      if (x === ' ') {
+        res = `${res}${x}`;
+        return x;
+      }
+      if (x.length > 5) {
+        const arr = [];
+        if (x.length % 5 !== 0) {
+          arr.push(x.slice(0, x.length % 5));
+        }
+        arr
+          .concat(x.slice(x.length % 5, x.length).match(/.{1,5}/g))
+          .map((y) => {
+            res = `${res}${MORSE_TABLE[y]}`;
+            return y;
+          });
+        return x;
+      }
+      res = `${res}${MORSE_TABLE[x]}`;
+      return x;
+    })
+    .join('');
+  return res.trim();
 };
